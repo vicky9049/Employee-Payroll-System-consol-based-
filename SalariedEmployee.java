2367e37bf927259e00ff01530c1cd65534835bf8
Vicky Sah <sahvicky603@gmail.com>
2025-09-24 18:42:16 +0530

    Create SalariedEmployee.java

---
+++ SalariedEmployee.java
@@ -0,0 +1,13 @@
+public class SalariedEmployee extends Employee {
+    private double monthlySalary;
+
+    public SalariedEmployee(String name, int id, double monthlySalary) {
+        super(name, id);
+        this.monthlySalary = monthlySalary;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return monthlySalary;
+    }
+}

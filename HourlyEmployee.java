b6dd8d1a3168532160380d9c4487c347979c99e0
Vicky Sah <sahvicky603@gmail.com>
2025-09-24 18:42:47 +0530

    Create HourlyEmployee.java

---
+++ HourlyEmployee.java
@@ -0,0 +1,15 @@
+public class HourlyEmployee extends Employee {
+    private double hourlyRate;
+    private int hoursWorked;
+
+    public HourlyEmployee(String name, int id, double hourlyRate, int hoursWorked) {
+        super(name, id);
+        this.hourlyRate = hourlyRate;
+        this.hoursWorked = hoursWorked;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return hourlyRate * hoursWorked;
+    }
+}
